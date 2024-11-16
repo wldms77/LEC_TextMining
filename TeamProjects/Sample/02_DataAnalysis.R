94fe26d227c21fe23c0695e33e70b3a57cdb95a9
Jiho Kang <22200022@handong.ac.kr>
2024-11-16 21:05:11 +0900

    Update 02_DataAnalysis.R

--- TeamProjects/Sample/02_DataAnalysis.R
+++ TeamProjects/Sample/02_DataAnalysis.R
@@ -1,1 +1,9 @@
+##################################################################
+### Text Mining Team Project                                   ###
+### Project Title:                                             ###
+### Project Goal:                                              ###
+### Project Members: Keungoui Kim (https://github.com/awekim)  ###
+###                  Sangsan Lee ( )                           ###
+##################################################################
+
 library(dplyr)
