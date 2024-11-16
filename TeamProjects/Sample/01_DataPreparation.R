8e8096e1f62c90990b1a8d2b3911c2608c77e193
Jiho Kang <22200022@handong.ac.kr>
2024-11-16 21:02:22 +0900

    Update 01_DataPreparation.R

--- TeamProjects/Sample/01_DataPreparation.R
+++ TeamProjects/Sample/01_DataPreparation.R
@@ -1,9 +1,11 @@
-##################################################################
-### Text Mining Team Project                                   ###
-### Project Title:                                             ###
-### Project Goal:                                              ###
-### Project Members: Keungoui Kim (https://github.com/awekim)  ###
-###                  Sangsan Lee ( )                           ###
-##################################################################
+####################################################################################
+### Text Mining Team Project                                                     ###
+### Project Title: High-Scoring Keyword Analysis Project for Job Interviews      ###
+### Project Goal: We identify high-scoring keywords to analyze interview         ### 
+###               success factors and provide data-driven insights.              ###
+### Project Members: YoungWoo Cho (https://github.com/awekim)                    ###
+###                  Jiho Kang ([hahahohoJIHO](https://github.com/hahahohoJIHO)) ###
+###                  Yewon Kim ([daydayyewon](https://github.com/daydayyewon))   ###
+####################################################################################
 
 library(dplyr)
